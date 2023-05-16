7ebaef2c33564161a0b579ff5bc3772259ef9c25
Joao Paulo <jpferreteg@gmail.com>
2023-05-16 20:09:26 -0300

    Revert "Revert "feat: add post model and repository""
    This reverts commit 0b01f33d6de88b7d2d9af761eec85c9e5dca969a.

---
+++ post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/PostRepository.kt
@@ -0,0 +1,9 @@
+package com.example.postmicrosservice.repository
+
+import com.example.postmicrosservice.model.Post
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PostRepository: CrudRepository<Post, String> {
+}
