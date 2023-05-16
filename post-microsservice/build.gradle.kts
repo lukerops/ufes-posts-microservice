0b01f33d6de88b7d2d9af761eec85c9e5dca969a
Joao Paulo <jpferreteg@gmail.com>
2023-05-16 20:07:50 -0300

    Revert "feat: add post model and repository"
    This reverts commit 082ce66ee5b7f39d1e2d0598127719eca463be5a.

--- post-microsservice/build.gradle.kts
+++ post-microsservice/build.gradle.kts
@@ -3,10 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.0.6"
 	id("io.spring.dependency-management") version "1.1.0"
-	id("org.jetbrains.kotlin.plugin.allopen") version "1.8.21"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
-	id("io.freefair.lombok") version "8.0.1"
 }
 
 group = "com.example"
@@ -22,12 +20,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.0.6")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-	testImplementation("net.datafaker:datafaker:1.8.0")
-	testImplementation("com.squareup.okhttp3:mockwebserver:4.9.1")
 }
 
 tasks.withType<KotlinCompile> {
--- post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/PostRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.postmicrosservice.repository
-
-import com.example.postmicrosservice.model.Post
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PostRepository: CrudRepository<Post, String> {
-}
