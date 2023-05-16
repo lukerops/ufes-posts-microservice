f713fd1067054801a54db8c416c913e8a9aab979
Joao Paulo <jpferreteg@gmail.com>
2023-05-16 16:36:25 -0300

    feat: add started project

---
+++ post-microsservice/src/main/kotlin/com/example/postmicrosservice/PostMicrosserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.postmicrosservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PostMicrosserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<PostMicrosserviceApplication>(*args)
+}
