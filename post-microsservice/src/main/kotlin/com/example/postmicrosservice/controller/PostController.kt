db933454a1d9ae47946106efd0d7c12adfe98462
brenoSN <breno.s.nascimento@edu.ufes.br>
2023-05-23 17:21:29 -0300

    Add get function

--- post-microsservice/src/main/kotlin/com/example/postmicrosservice/controller/PostController.kt
+++ post-microsservice/src/main/kotlin/com/example/postmicrosservice/controller/PostController.kt
@@ -1,13 +1,20 @@
 package com.example.postmicrosservice.controller
 
+import com.example.postmicrosservice.model.Post
 import com.example.postmicrosservice.service.PostService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.http.HttpResponse
 
 @RestController
 @RequestMapping("/post")
 class PostController(
     private val postService: PostService
 ) {
+
+    @GetMapping("/get/{id}")
+    fun get(@PathVariable id: Long): Post? =
+        postService.getPost(id)
 
 }
