9be4a6c7e9c302bd055d98229ac9b891beb88490
Lázaro José Pedrosa dos Reis <lazaro.pedrosa@outlook.com>
2023-06-06 20:55:14 -0300

    Changed the return type
    Post to a list of media

--- post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
+++ post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Repository
 @Repository
 interface MediaRepository: CrudRepository<Media, Long> {
 
-    fun findByMediaId(mediaId: Long): Post?
+    fun findByMediaId(postId: Long): List<Media>
 
     fun findByPostId(postId: Long): Media?
 
 }
+
+
