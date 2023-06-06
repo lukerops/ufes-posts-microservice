4048b48d08bc4abdfaa2b3d0f1d6542b21874f59
Lázaro José Pedrosa dos Reis <lazaro.pedrosa@outlook.com>
2023-06-06 19:07:13 -0300

    Update MediaRepository.kt

--- post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
+++ post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
@@ -1,7 +1,14 @@
+package com.example.postmicrosservice.repository
+
+import com.example.postmicrosservice.model.Media
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
 @Repository
 interface MediaRepository: CrudRepository<Media, Long>{
-
     fun findByMediaId(mediaId: Long): List<Media>
+
+    fun findByPostId(postId: Long): Media?
 
 }
 
