8fbe23dd2a7d91d94f0b86a0ddd70c9881cd972a
Lázaro José Pedrosa dos Reis <lazaro.pedrosa@outlook.com>
2023-06-07 14:25:11 -0300

    Revert "Update MediaRepository.kt"
    This reverts commit 4048b48d08bc4abdfaa2b3d0f1d6542b21874f59.

--- post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
+++ post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
@@ -1,14 +1,7 @@
-package com.example.postmicrosservice.repository
-
-import com.example.postmicrosservice.model.Media
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
 @Repository
 interface MediaRepository: CrudRepository<Media, Long>{
-    fun findByMediaId(mediaId: Long): List<Media>
 
-    fun findByPostId(postId: Long): Media?
+    fun findByMediaId(mediaId: Long): List<Media>
 
 }
 
