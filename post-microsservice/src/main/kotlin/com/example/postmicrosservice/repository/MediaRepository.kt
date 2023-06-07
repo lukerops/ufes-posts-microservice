40afe76e5d1fc33b5fa464a9c8ce61a026cee953
Lázaro José Pedrosa dos Reis <lazaro.pedrosa@outlook.com>
2023-06-07 14:24:59 -0300

    Revert "Changed return type"
    This reverts commit 4ecc9f47b45a5da5096678c001c0dd068b4538f7.

--- post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
+++ post-microsservice/src/main/kotlin/com/example/postmicrosservice/repository/MediaRepository.kt
@@ -1,15 +1,14 @@
 package com.example.postmicrosservice.repository
 
 import com.example.postmicrosservice.model.Media
-import com.example.postmicrosservice.model.Post
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MediaRepository: CrudRepository<Media, Long> {
-
-    fun findByMediaId(mediaId: Long): Post?
+interface MediaRepository: CrudRepository<Media, Long>{
+    fun findByMediaId(mediaId: Long): List<Media>
 
     fun findByPostId(postId: Long): Media?
 
 }
+
